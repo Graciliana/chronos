8e102667df74668c5529fec7f8e8bfa093ce032d
Graciliana Kascher <eagraciliana@gmail.com>
2025-10-30 15:06:24 -0300

    [feat]  components heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import './styles/theme.css';
 import './styles/global.css';
+import './styles/theme.css';
+
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
     
     return (
         <>
-            <Heading />
+            <Heading >A Game of Thrones
+                <button><TimerIcon />
+                </button>
+            </Heading>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt optio aliquid consequuntur illo vero omnis vitae minus officiis ut, pariatur iure cumque natus numquam tenetur cum eum. Provident, accusantium corporis.</p>
         </>
     );
-    
- 
 }
 
 
