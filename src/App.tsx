d8136299c2e784e7c1d24891cde6292c92615f0a
Graciliana Kascher <eagraciliana@gmail.com>
2025-11-04 10:36:35 -0300

    [feat]  organizing the components into standardized folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 import './styles/global.css';
 import './styles/theme.css';
-
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
     
     return (
-        <>
-            <Heading >A Game of Thrones
-                <button><TimerIcon />
-                </button>
-            </Heading>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt optio aliquid consequuntur illo vero omnis vitae minus officiis ut, pariatur iure cumque natus numquam tenetur cum eum. Provident, accusantium corporis.</p>
+      <>
+        <Container>
+          <Heading>LOGO</Heading>
+        </Container>
+
+        <Container>
+          <Heading>Menu</Heading>
+        </Container>
+
         </>
     );
 }
