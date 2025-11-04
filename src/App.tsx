cf0757176699b0a890c65a22d6d09cec2aef2774
Graciliana Kascher <eagraciliana@gmail.com>
2025-11-04 15:45:00 -0300

    [feat] Create form

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,43 @@ import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-    
-    return (
-      <>
-        <Container>
-          <Logo />
-        </Container>
 
-        <Container>
-          <Menu/>
-        </Container>
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
 
-          <Container>
-          <CountDown/>
-        </Container>
+      <Container>
+        <Menu />
+      </Container>
 
-        </>
-    );
+      <Container>
+        <CountDown />
+      </Container>
+       <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
 }
 
 
